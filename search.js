2dc0f5beacb7f9166cf1bc111008683c2a4e036e
Oli <oli@hoodieboi.dev>
2024-01-15 20:08:16 +0100

    Improve search bar

--- search.js
+++ search.js
@@ -24,9 +24,11 @@ function registerFuse(list) {
     const fuse = new Fuse(list, {
         keys: [
             'title',
-            'author',
             'content',
-        ]
+            'author',
+        ],
+        findAllMatches: true,
+        threshold: 0.5,
     })
 
     searchBar?.addEventListener('keydown', e => {
@@ -89,4 +91,50 @@ function goToSearch() {
 function hideSearchResults() {
     searchResults.ariaHidden = 'true'
     searchResults.innerHTML = ''
+}
+
+// Disable scroll when hovering over results
+searchResults.addEventListener('mouseenter', disableScroll)
+searchResults.addEventListener('mouseleave', enableScroll)
+
+// Code from https://stackoverflow.com/a/4770179
+// left: 37, up: 38, right: 39, down: 40,
+var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+function preventDefault(e) {
+  e.preventDefault();
+}
+
+function preventDefaultForScrollKeys(e) {
+  if (keys[e.keyCode]) {
+    preventDefault(e);
+    return false;
+  }
+}
+
+// modern Chrome requires { passive: false } when adding event
+var supportsPassive = false;
+try {
+  window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+    get: function () { supportsPassive = true; } 
+  }));
+} catch(e) {}
+
+var wheelOpt = supportsPassive ? { passive: false } : false;
+var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+
+// call this to Disable
+function disableScroll() {
+  window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+  window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+  window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+  window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
+// call this to Enable
+function enableScroll() {
+  window.removeEventListener('DOMMouseScroll', preventDefault, false);
+  window.removeEventListener(wheelEvent, preventDefault, wheelOpt); 
+  window.removeEventListener('touchmove', preventDefault, wheelOpt);
+  window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
 }
